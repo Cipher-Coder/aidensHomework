f6f5bdf9360e7648dfca19564af2af879cfec157
Cipher-Coder <matthew.rebehn@gmail.com>
2019-03-10 12:24:23 -0400

    Made nav tabs on vocab page

--- assets/main.js
+++ assets/main.js
@@ -115,3 +115,22 @@ function startLoading() {
   generateRandomAdd();
   generateRandomSub();
 }
+
+// Tab Content on Vocab Page
+function openName(evt, tabName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Default tab to be open
+document.getElementById("defaultOpen").click();
