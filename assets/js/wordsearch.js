4d1c34942387ad6d13eb24688b2b02eeac18acef
Cipher-Coder <matthew.rebehn@gmail.com>
2019-04-02 11:00:57 -0400

    took orientations out to make puzzle easier

--- assets/js/wordsearch.js
+++ assets/js/wordsearch.js
@@ -34,16 +34,7 @@
      */
 
     // The list of all the possible orientations
-    var allOrientations = [
-      "horizontal",
-      "horizontalBack",
-      "vertical",
-      "verticalUp",
-      "diagonal",
-      "diagonalUp",
-      "diagonalBack",
-      "diagonalUpBack"
-    ];
+    var allOrientations = ["horizontal", "vertical", "diagonal"];
 
     // The definition of the orientation, calculates the next square given a
     // starting square (x,y) and distance (i) from that square.
